2f73b4109df75d661f29d1f303541c0056885dfe
prem-prasad <pr20090066870@gmail.com>
2025-04-24 20:12:19 +0530

    Redirect Loop solve!

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -3,7 +3,7 @@ import { isAuthenticated } from '@/lib/actions/auth.action'
 import { redirect } from 'next/navigation';
 const AuthLayout = async({children}:{children:ReactNode}) => {
     const isuserAuthenticated = await isAuthenticated();
-    if(!isuserAuthenticated) {
+    if(isuserAuthenticated) {
       redirect('/');
     }
   return (
