953b62b478c703ef7a0df85a107371551138a631
prem-prasad <pr20090066870@gmail.com>
2025-04-25 17:46:29 +0530

    feedback and interview generation!

--- components/InterviewCard.tsx
+++ components/InterviewCard.tsx
@@ -5,9 +5,10 @@ import Image from 'next/image'
 import { Button } from '@/components/ui/button'
 import Link from 'next/link'
 import DisplayTechIcons from './DisplayTechIcons';
+import { getFeedbackByInterviewId } from '@/lib/actions/general.action';
 
-const InterviewCard = ({interviewId ,userId, role, type, techstack, createdAt }: InterviewCardProps) => {
-    const feedback = null as Feedback | null;
+const InterviewCard = async({id ,userId, role, type, techstack, createdAt }: InterviewCardProps) => {
+    const feedback = userId && id ? await getFeedbackByInterviewId({ interviewId: id, userId }) : null;
     const normalizedType =/mix/gi.test(type) ? "Mixed" : type;
     const formattedDate = dayjs(feedback?.createdAt || createdAt || Date.now()).format('MMMM D, YYYY')
 
@@ -39,7 +40,7 @@ const InterviewCard = ({interviewId ,userId, role, type, techstack, createdAt }:
         <div className='flex flex-row justify-between'>
           <DisplayTechIcons techStack={techstack} />
           <Button className="btn-primary">
-            <Link href={feedback ? `/interview/${interviewId}/feedback` : `/interview/${interviewId}`}>
+            <Link href={feedback ? `/interview/${id}/feedback` : `/interview/${id}`}>
             {feedback ? 'Check Feedback' : 'view Interview'}
             </Link>
           </Button>
